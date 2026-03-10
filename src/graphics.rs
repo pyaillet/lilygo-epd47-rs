e96c76b9802eac4ea0c3be2540168bec684bdd93
Pierre-Yves Aillet <pyaillet@gmail.com>
2026-03-10 21:43:21 +0100

    feat: implement rotation for the display

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,6 +1,9 @@
 use embedded_graphics_core::{pixelcolor::Gray4, prelude::*};
 
-use crate::{display::Display, Error};
+use crate::{
+    display::{Display, DisplayRotation},
+    Error,
+};
 
 impl<'a> DrawTarget for Display<'a> {
     type Color = Gray4;
@@ -12,7 +15,8 @@ impl<'a> DrawTarget for Display<'a> {
         I: IntoIterator<Item = Pixel<Self::Color>>,
     {
         for Pixel(coord, color) in pixels.into_iter() {
-            let result = self.set_pixel(coord.x as u16, coord.y as u16, color.luma());
+            let (x, y) = translate_coord_rotation(coord.x as u16, coord.y as u16, &self.rotation());
+            let result = self.set_pixel(x, y, color.luma());
             if matches!(result, Err(Error::OutOfBounds)) {
                 continue;
             }
@@ -26,9 +30,26 @@ impl<'a> DrawTarget for Display<'a> {
     }
 }
 
+#[inline(always)]
+fn translate_coord_rotation(x: u16, y: u16, rotation: &DisplayRotation) -> (u16, u16) {
+    match rotation {
+        DisplayRotation::Rotate0 => (x, y),
+        DisplayRotation::Rotate90 => (Display::WIDTH - 1 - y, x),
+        DisplayRotation::Rotate180 => (Display::WIDTH - 1 - x, Display::HEIGHT - 1 - y),
+        DisplayRotation::Rotate270 => (y, Display::HEIGHT - 1 - x),
+    }
+}
+
 impl<'a> OriginDimensions for Display<'a> {
     fn size(&self) -> Size {
-        Size::new(Self::WIDTH as u32, Self::HEIGHT as u32)
+        match self.rotation() {
+            DisplayRotation::Rotate0 | DisplayRotation::Rotate180 => {
+                Size::new(Self::WIDTH as u32, Self::HEIGHT as u32)
+            }
+            DisplayRotation::Rotate90 | DisplayRotation::Rotate270 => {
+                Size::new(Self::HEIGHT as u32, Self::WIDTH as u32)
+            }
+        }
     }
 }
 
